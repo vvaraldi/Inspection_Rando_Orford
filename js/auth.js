5be1c744fdc3b6b750309519c61dc211d6a83cb1
vvaraldi <vincent.varaldi@gmail.com>
2025-08-08 00:28:59 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -33,26 +33,36 @@ function checkAuthStatus() {
     if (user) {
 
 
+    const inspectorDoc = await db.collection('inspectors').doc(user.uid).get();
+    if (inspectorDoc.exists) {
+		  const userData = inspectorDoc.data();
+		  
+		  // Vérifier si l'utilisateur est actif
+		  if (userData.status !== 'active') {
+			alert('Votre compte a été désactivé. Contactez l'administrateur.');
+			auth.signOut();
+			return;
+		  }
 
 
 
 
-
-      // Utilisateur connecté
-      console.log("Utilisateur connecté:", user.email);
-      
-      // Mettre à jour le lien de connexion pour afficher "Déconnexion"
-      if (loginLink) {
-        loginLink.textContent = 'Déconnexion';
-        loginLink.onclick = function(e) {
-          e.preventDefault();
-          auth.signOut().then(() => {
-            window.location.reload();
-          }).catch(error => {
-            console.error("Erreur lors de la déconnexion:", error);
-          });
-        };
-      }
+		  // Utilisateur connecté
+		  console.log("Utilisateur connecté:", user.email);
+		  
+		  // Mettre à jour le lien de connexion pour afficher "Déconnexion"
+		  if (loginLink) {
+			loginLink.textContent = 'Déconnexion';
+			loginLink.onclick = function(e) {
+			  e.preventDefault();
+			  auth.signOut().then(() => {
+				window.location.reload();
+			  }).catch(error => {
+				console.error("Erreur lors de la déconnexion:", error);
+			  });
+			};
+		  }
+	  }
       
       // Faire de même pour le lien mobile
       if (mobileLoginLink) {
